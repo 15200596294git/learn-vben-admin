0521c8eec3ca2b1b0b5d1cc2a88eefadd90df7b9
lwj <jg15575446613@163.com>
2023-11-23 18:05:00 +0800

    新建vite项目

---
+++ vite-project/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+console.log(process.env.NODE_ENV)
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/lwj/',
+  plugins: [vue()],
+})
